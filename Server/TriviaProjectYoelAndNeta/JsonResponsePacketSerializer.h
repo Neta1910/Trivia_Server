60cfc78e9b2c8a20d2677d0ccd061d6df6c374ef
Yoel_Weisberg5 <thek57525@gmail.com>
2024-04-01 23:33:30 +0300

    added ErrorResponse struct

--- Server/TriviaProjectYoelAndNeta/JsonResponsePacketSerializer.h
+++ Server/TriviaProjectYoelAndNeta/JsonResponsePacketSerializer.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "pch.h"
+#include "json.hpp"
+#include "ResponseStructs.h"
+
+using json = nlohmann::json;
 
 class JsonResponsePacketSerialize
 {
 public:
-
+	static std::vector<unsigned char> serializeLoginResponse(const LoginResponse& response);
+	static std::vector<unsigned char> serializeSignUpResponse(const SignupResponse& response);
+	static std::vector<unsigned char> serializeErrorResponse(const  & response);
 };
