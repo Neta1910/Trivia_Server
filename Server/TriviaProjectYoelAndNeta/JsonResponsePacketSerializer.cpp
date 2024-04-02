6255d1048e51a3691b47423045900e29a85da616
Yoel_Weisberg5 <thek57525@gmail.com>
2024-04-02 16:45:56 +0300

    created some helper functions, a function to turn an int into abinary number, in a function to pad zeors

--- Server/TriviaProjectYoelAndNeta/JsonResponsePacketSerializer.cpp
+++ Server/TriviaProjectYoelAndNeta/JsonResponsePacketSerializer.cpp
@@ -1,4 +1,9 @@
 #include "JsonResponsePacketSerializer.h"
+#define MESSEGE_SIZE_TO_SIZE 5
+#define BYTE 8
+#define CODE_LENGTH 1
+#define MESSEGE_LENGTH_LENGTH 4
+
 
 std::vector<unsigned char> JsonResponsePacketSerialize::serializeLoginResponse(const LoginResponse& response)
 {
@@ -6,7 +11,7 @@ std::vector<unsigned char> JsonResponsePacketSerialize::serializeLoginResponse(c
     std::string json_str = j.dump();
     // Convert string to bytes
     std::vector<unsigned char> bytes(json_str.begin(), json_str.end());
-    return bytes;
+    return JsonResponsePacketSerialize::parseDataIntoMessage(bytes, CODE_LOGIN_RESP) ;
 }
 
 std::vector<unsigned char> JsonResponsePacketSerialize::serializeSignUpResponse(const SignupResponse& response)
@@ -15,7 +20,7 @@ std::vector<unsigned char> JsonResponsePacketSerialize::serializeSignUpResponse(
     std::string json_str = j.dump();
     // Convert string to bytes
     std::vector<unsigned char> bytes(json_str.begin(), json_str.end());
-    return bytes;
+    return JsonResponsePacketSerialize::parseDataIntoMessage(bytes, CODE_SIGN_UP_RESP);
 }
 
 std::vector<unsigned char> JsonResponsePacketSerialize::serializeErrorResponse(const ErrorResponse& response)
@@ -24,7 +29,7 @@ std::vector<unsigned char> JsonResponsePacketSerialize::serializeErrorResponse(c
     std::string json_str = j.dump();
     // Convert string to bytes
     std::vector<unsigned char> bytes(json_str.begin(), json_str.end());
-    return bytes;
+    return JsonResponsePacketSerialize::parseDataIntoMessage(bytes, CODE_ERROR_RESPONSE);
 }
 
 std::vector<unsigned char> JsonResponsePacketSerialize::parseDataIntoMessage(const std::vector<unsigned char>& data, const int& respCode)
@@ -32,10 +37,43 @@ std::vector<unsigned char> JsonResponsePacketSerialize::parseDataIntoMessage(con
     std::vector<unsigned char> res;
 
     // putting the code inside
-    res.push_back(static_cast<unsigned char>(respCode));
-
-
+    std::vector<unsigned char> code = JsonResponsePacketSerialize::turnIntToBinary(respCode);
+    res.insert(res.end(), code.begin(), code.end());
     
+
+    // getting message size 
+    res.insert(res.end(), data.begin(), data.end());
+    while (res.size() < MESSEGE_SIZE_TO_SIZE)
+    {
+        res.push_back(0);
+    }
+
+    // putting in the messege data
+    res.insert(res.end(), data.begin(), data.end());
+
+    return res;
+}
+
+std::vector<unsigned char>  JsonResponsePacketSerialize::turnIntToBinary(const int& num,const  int size)
+{
+    std::vector<unsigned char> binary;
+    int temp = num;
+    while (temp > 0) {
+        int remainder = num % 2;
+        binary.push_back(remainder);
+        temp /= 2;
+    }
+    // Reverse the binary vector to get the correct binary representation
+    std::reverse(binary.begin(), binary.end());
+    return binary;
+}
+
+void JsonResponsePacketSerialize::padWithZeros(std::vector<unsigned char>* data, const int& amountOfZeroz)
+{
+    while (data->size() <= amountOfZeroz)
+    {
+        data->insert(data->begin(), 0);
+    }
 }
 
 
--- Server/TriviaProjectYoelAndNeta/JsonResponsePacketSerializer.h
+++ Server/TriviaProjectYoelAndNeta/JsonResponsePacketSerializer.h
@@ -12,4 +12,6 @@ public:
 	static std::vector<unsigned char> serializeSignUpResponse(const SignupResponse& response);
 	static std::vector<unsigned char> serializeErrorResponse(const  ErrorResponse& response);
 	static std::vector<unsigned char> parseDataIntoMessage(const std::vector<unsigned char>& data, const int& respCode );
+	static std::vector<unsigned char> turnIntToBinary(int& num, const int size);
+	static void padWithZeros(std::vector<unsigned char>* data, const int& amountOfZeroz);
 };
