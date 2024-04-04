5fa856556bdc0c54adacfd5a52f7ebed0a1a8a60
Neta Orevi <Netaorevi1910@gmail.com>
2024-04-04 18:55:20 +0300

    Done implementing class 'LoginManager'

--- Server/TriviaProjectYoelAndNeta/LoginManager.cpp
+++ Server/TriviaProjectYoelAndNeta/LoginManager.cpp
@@ -22,7 +22,45 @@ void LoginManager::login(std::string username, std::string password)
 	m_loggedUsers.push_back(loggedUser);
 }
 
+void LoginManager::logout(std::string username)
+{
+	int counter = 0;
+	std::vector<LoggedUser>::iterator it;
+	for (it = m_loggedUsers.begin(); it != m_loggedUsers.end(); ++it)
+	{
+		if ((*it).getUsername() == username)
+		{
+			m_loggedUsers.erase(it);
+		}
+		counter++;
+	}
+}
+
 bool LoginManager::open()
 {
-	m_database->open();
+	return m_database->open();
+}
+
+bool LoginManager::close()
+{
+	return m_database->close();
+}
+
+int LoginManager::doesUserExist(std::string username)
+{
+	return m_database->doesUserExist(username);
+}
+
+int LoginManager::DoesPasswordMatch(std::string username, std::string password)
+{
+	return m_database->DoesPasswordMatch(username, password);
+}
+
+int LoginManager::addNewUser(std::string username, std::string password, std::string email)
+{
+	if (!m_database->doesUserExist(username))
+	{
+		return m_database->addNewUser(username, password, email);
+	}
+	return ERROR_RETURN_CODE;
 }
