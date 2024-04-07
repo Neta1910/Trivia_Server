b5967d960e23847748a72c1e2ea04eb95ab4cb10
Neta Orevi <Netaorevi1910@gmail.com>
2024-04-07 15:08:12 +0300

    Wrote header file for class 'RequestHandlerFactory'

--- Server/TriviaProjectYoelAndNeta/RequestHandlerFactory.h
+++ Server/TriviaProjectYoelAndNeta/RequestHandlerFactory.h
@@ -1,6 +1,13 @@
 #pragma once
+#include "LoginRequestHandler.h"
+#include "LoginManager.h"
 
 class RequestHandlerFactory
 {
-
+public:
+	LoginRequestHandler* createLoginRequestHandler();
+	LoginManager GetLoginManager();
+private: 
+	LoginManager m_loginManager;
+	IDatabase* m_database;
 };
