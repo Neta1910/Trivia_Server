c62fd9cbe951ed9f5305b3ce21a3913bbc6c7a9b
Neta Orevi <Netaorevi1910@gmail.com>
2024-04-12 11:36:00 +0300

    Implemented almost all of class 'MenuRequestHandler'

--- Server/TriviaProjectYoelAndNeta/MenuRequestHandler.cpp
+++ Server/TriviaProjectYoelAndNeta/MenuRequestHandler.cpp
@@ -51,5 +51,48 @@ RequestResult MenuRequestHandler::getRooms(RequestInfo& reqInfo)
 
 RequestResult MenuRequestHandler::getPlayersInRoom(RequestInfo& reqInfo)
 {
-	//GetPlayersInRoomResponse getPlayersInRoom_res = JsonRequestPacketDeserializer::de
+	GetPlayersInRoomRequest getPlayersInRoom_req = JsonRequestPacketDeserializer::deserializeGetPlayersInRoomRequest(reqInfo.buffer);
+	if (!m_roomManager.doesRoomExist(getPlayersInRoom_req.roomId))
+	{
+		ErrorResponse error_res = { "Room doesn't exist" };
+		return { JsonResponsePacketSerialize::serializeErrorResponse(error_res), (IRequestHandler*)m_handleFactory.createMenuRequestHandler(m_user.getUsername()) };
+	}
+	GetPlayersInRoomResponse getPlayersRoom_res = { GET_PLAYERS_RESP, m_roomManager.getRoom(getPlayersInRoom_req.roomId).getAllUsers() };
+	return { JsonResponsePacketSerialize::serializeGetPlayersInRoomResponse(getPlayersRoom_res), (IRequestHandler*)m_handleFactory.createMenuRequestHandler(m_user.getUsername()) };
 }
+
+RequestResult MenuRequestHandler::getPersonalStats(RequestInfo& reqInfo)
+{
+	GetPersonalStatsResponse getPersonalStats_res = { GET_PERSONAL_STATS_RESP, m_handleFactory.getStatisticsManager().getUserStatistics(m_user.getUsername()) };
+	return { JsonResponsePacketSerialize::serializeGetPersonalStatsResponse(getPersonalStats_res), (IRequestHandler*)m_handleFactory.createMenuRequestHandler(m_user.getUsername()) };
+}
+
+RequestResult MenuRequestHandler::getHighScore(RequestInfo& reqInfo)
+{
+	GetHighScoreResponse getHighScore_res = { GET_HIGH_SCORE_RESP, m_handleFactory.getStatisticsManager().getHighScore() };
+	return { JsonResponsePacketSerialize::serializeHighScoreResponse(getHighScore_res), (IRequestHandler*)m_handleFactory.createMenuRequestHandler(m_user.getUsername()) };
+}
+
+RequestResult MenuRequestHandler::joinRoom(RequestInfo& reqInfo)
+{
+	JoinRoomRequest joinRoom_req = JsonRequestPacketDeserializer::deserializeJoinRoomRequest(reqInfo.buffer);
+	ErrorResponse error_res;
+	if (m_roomManager.doesRoomExist(joinRoom_req.roomId))
+	{
+		// Check if room is full
+		if (m_roomManager.getRoom(joinRoom_req.roomId).getRoomData().maxPlayers == m_roomManager.getRoom(joinRoom_req.roomId).getAllUsers().size())
+		{
+			error_res = { "room is full!" };
+		}
+		else // Join player to room if there's enough space
+		{
+			m_roomManager.getRoom(joinRoom_req.roomId).addUser(m_user);
+			JoinRoomResponse joinRoom_res = { JOIN_ROOM_RESP };
+			//return { JsonResponsePacketSerialize::serializeJoinRoomResponse(joinRoom_res), (IRequestHandler*)m_handleFactory.createMenuRequestHandler() };
+		}
+	}
+	error_res = { "Room doesn't exist" };
+	return { JsonResponsePacketSerialize::serializeErrorResponse(error_res), (IRequestHandler*)m_handleFactory.createMenuRequestHandler(m_user.getUsername()) };
+
+}
+
