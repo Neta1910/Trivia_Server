66a0e80e631c150d1421a86c7f513fea333b425a
Neta Orevi <Netaorevi1910@gmail.com>
2024-04-16 21:55:15 +0300

    Added files for class 'RoomAdminRequestHandler'

---
+++ Server/TriviaProjectYoelAndNeta/RoomAdminRequestHandler.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include "IRequestHandler.h"
+#include "Room.h"
+#include "RequestHandlerFactory.h"
+#include "LoggedUser.h"
+
+class RoomAdminRequestHandler : public IRequestHandler
+{
+public:
+	RoomAdminRequestHandler(RequestHandlerFactory& handleFactory, std::string roomAdmin, RoomManager& roomManager);
+	virtual bool isRequestRelevant(RequestInfo& reqInfo) override;
+	virtual RequestResult handleRequest(RequestInfo& reqInfo) override;
+private:
+
+	RequestResult closeRoom(RequestInfo& reqInfo);
+	RequestResult startGame(RequestInfo& reqInfo);
+	RequestResult getRoomState(RequestInfo& reqInfo);
+
+	Room m_room;
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	RequestHandlerFactory& m_handlerFactory;
+};
+
