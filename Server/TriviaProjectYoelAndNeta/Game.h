650df49b70c99102380c0cf8d04a14c2da4c5782
Neta Orevi <Netaorevi1910@gmail.com>
2024-04-23 21:36:45 +0300

    Added files for class 'Game'

---
+++ Server/TriviaProjectYoelAndNeta/Game.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Quistion.h"
+#include <map>
+#include "LoggedUser.h"
+
+typedef struct GameData
+{
+	Question currentQuestion;
+	unsigned int correctAnswerCount;
+	unsigned int wrongAnswerCount;
+	unsigned int averageAnswerTime;
+};
+
+class Game
+{
+public:
+	Question getQuestionForUser(LoggedUser user);
+	void submitAnswer(LoggedUser user, unsigned int answer);
+	void removePlayer(LoggedUser user);
+private:
+	std::vector<Question> m_questions;
+	std::map<LoggedUser, GameData> m_players;
+	unsigned int gameId;
+
+	void submitGameStatsToDB(GameData game_data);
+};
