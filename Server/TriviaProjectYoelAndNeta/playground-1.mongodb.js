5ad0ff3990cf527730998778c39a58b998c63419
Neta Orevi <Netaorevi1910@gmail.com>
2024-06-06 21:42:25 +0300

    Added mangoDB functions implementation file

---
+++ Server/TriviaProjectYoelAndNeta/playground-1.mongodb.js
@@ -0,0 +1,10 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use('Trivia-0');
+
+// Create a new document in the collection.
+db.getCollection('0.1.Users').insertOne({
+
+});
